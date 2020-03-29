05b02e4ca7b9de6f1f6e1138f3fff0297753a9c2
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-03-29 22:38:08 +0530

    Added searchMoviesSeenByUserBySingleGenre() method

--- dao/movieGenreDAO.py
+++ dao/movieGenreDAO.py
@@ -34,6 +34,28 @@ class MovieGenreDAO():
         return output
 
     # Return a list of MovieGenre objects
+    # containing movies seen by a user with userId as argument
+    # for a single genre from user_movie_genre_info VIEW
+    def searchMoviesSeenByUserBySingleGenre(self, userId:int, genreId:int) -> List[MovieGenre]:
+        output = []
+        query = '''
+            SELECT movieId
+            FROM user_movie_genre_info
+            WHERE userId = {0} AND genreId = {1}
+            
+            EXCEPT
+            
+            SELECT movieId
+            FROM user_movie_genre_info
+            WHERE userId = {0} AND genreId != {1}
+        '''.format(userId, genreId)
+        cursor = self.myConn.execute(query)
+        for row in cursor:
+            new_row = list(row) + genreId
+            output.append(self.convertRowToMovieGenre(new_row))
+        return output
+
+    # Return a list of MovieGenre objects
     # containing rows which have the genreId
     # column same as the argument
     def searchByGenreID(self, genreId:int) -> List[MovieGenre]:
@@ -77,6 +99,8 @@ class MovieGenreDAO():
 if __name__ == "__main__":
     dao = MovieGenreDAO("../config/db_properties.json")
 
+    print(dao.searchMoviesSeenByUserBySingleGenre(1, 3))
+
     # table = dao.getAllMovieGenres()
 
     # for row in table:
@@ -88,11 +112,11 @@ if __name__ == "__main__":
     # for movie_genre in dao.searchByGenreID(1):
     #     print(movie_genre)
 
-    from core.rating import Rating, Rating, Rating, Rating
+    # from core.rating import Rating
 
     # rating1 = Rating(1, 553, 5.0)
 
-    genres = dao.searchByUserRatingList(dao.searchByGenreID(1))
+    # genres = dao.searchByUserRatingList(dao.searchByGenreID(1))
 
-    for genre in genres:
-        print(genre)
+    # for genre in genres:
+    #     print(genre)
