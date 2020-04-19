297b43f812ff824b288776ca25f3131deede03ca
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-19 23:48:27 +0530

    Added get_non_zero_movie_genres() function

--- get_data/get_user_movie_count_data.py
+++ get_data/get_user_movie_count_data.py
@@ -7,3 +7,14 @@ import constant_paths
 def get_genre_wise_movie_count_by_user(user_id: int) -> List[UserMovieCountDAO]:
     dao = UserMovieCountDAO(constant_paths.CONFIG_FILE_PATH)
     return dao.searchByUserID(user_id)[0]
+
+
+def get_non_zero_movie_genres(user_id: int) -> List[int]:
+    genre_info = get_genre_wise_movie_count_by_user(user_id)
+    genre_list = []
+    genre_id = 1
+    for genre_count in genre_info.genreCounts:
+        if genre_count > 0:
+            genre_list.append(genre_id)
+        genre_id += 1
+    return genre_list.copy()
