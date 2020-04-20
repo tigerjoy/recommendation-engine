b71b97ab039a04e8e2673269b380a44b4bcb8f66
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-20 15:37:09 +0530

    Added get_count_of_movies_of_genres_not_seen_by_user() function

--- get_data/get_average_rating_data.py
+++ get_data/get_average_rating_data.py
@@ -5,15 +5,21 @@ from typing import List
 import constant_paths
 
 
-def get_movies_of_genres_seen_by_user(genre_list: List[Genre], user_id: int, ascending: bool = False) -> List[AverageRating]:
+def get_movies_of_genres_seen_by_user(genre_list: List[Genre], user_id: int, ascending: bool = False) -> List[
+    AverageRating]:
     dao = AverageRatingDAO(constant_paths.CONFIG_FILE_PATH)
     return dao.moviesSeenByUser(genre_list, user_id, ascending)
 
 
-def get_movies_of_genres_not_seen_user(genre_list: List[Genre], user_id: int, ascending: bool = False) -> List[AverageRating]:
+def get_movies_of_genres_not_seen_user(genre_list: List[Genre], user_id: int, ascending: bool = False) -> List[
+    AverageRating]:
     movies_from_users_favourite_genre = get_movies_by_genres(genre_list, ascending)
     users_seen_movies = get_movies_of_genres_seen_by_user(genre_list, user_id, ascending)
-    return set(movies_from_users_favourite_genre).difference(users_seen_movies)
+    return set(movies_from_users_favourite_genre).difference(set(users_seen_movies))
+
+
+def get_count_of_movies_of_genres_not_seen_by_user(genre_list: List[Genre], user_id: int) -> int:
+    return len(get_movies_of_genres_not_seen_user(genre_list, user_id))
 
 
 def get_movies_by_genres(genre_list: List[Genre], ascending: bool = False) -> List[AverageRating]:
