a44cce6be8b59a42cb9fd4c315903e8c9da4c9bc
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-26 00:19:49 +0530

    Fixed the issue of list index out of bounds, by checking the length of genre_ids

--- add_data/add_temp_recommendation_data.py
+++ add_data/add_temp_recommendation_data.py
@@ -37,6 +37,7 @@ def add_temp_recommendation(user_id: int, last_combination: int, priority: int, 
 #     }
 # ]
 def add_temp_recommendations(user_id: int, last_combination: int, temp_recommendations: list):
+    # print("baba")
     result = True
     #  Checking if records for user_id exists
     if len(gtr.get_temp_recommendation_by_user(user_id)) != 0:
@@ -48,7 +49,10 @@ def add_temp_recommendations(user_id: int, last_combination: int, temp_recommend
             combination_num = recommendation["combination_num"]
             common_movie_length = recommendation["common_movie_length"]
             genre_ids = recommendation["common_genres"]
-            genre_str = ",".join([str(x) for x in genre_ids])
+            if len(genre_ids) != 0:
+                genre_str = ",".join([str(x) for x in genre_ids])
+            else:
+                genre_str = ""
             result = (result and add_temp_recommendation(user_id, last_combination, priority, combination_num,
                                                          common_movie_length, genre_str))
 
