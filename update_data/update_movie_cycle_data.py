0d9c898b5b4d13c39371a8865881b464a61c5634
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-26 21:14:50 +0530

    Added priority in parameter

--- update_data/update_movie_cycle_data.py
+++ update_data/update_movie_cycle_data.py
@@ -3,8 +3,8 @@ from core.movieCycle import MovieCycle
 import constant_paths
 
 
-def update_movie_cycle_data(user_id: int, last_movie_id: int, second_last_movie_id: int) -> bool:
-    the_record = MovieCycle(user_id, last_movie_id, second_last_movie_id)
+def update_movie_cycle_data(user_id: int, last_movie_id: int, second_last_movie_id: int, priority: int) -> bool:
+    the_record = MovieCycle(user_id, last_movie_id, second_last_movie_id, priority)
     dao = MovieCycleDAO(constant_paths.CONFIG_FILE_PATH)
 
     return dao.updateRecordByUser(the_record)
