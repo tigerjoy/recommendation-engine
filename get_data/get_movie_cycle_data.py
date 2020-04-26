e4e044e543b286ca98ce097c31e2d0c2d3dec03e
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-26 21:14:22 +0530

    Added priority in parameter

--- get_data/get_movie_cycle_data.py
+++ get_data/get_movie_cycle_data.py
@@ -1,9 +1,10 @@
 from dao.movieCycleDAO import MovieCycleDAO
 from core.movieCycle import MovieCycle
+from typing import List
 import constant_paths
 
 
-def get_movie_cycle_data(user_id: int) -> MovieCycle:
+def get_movie_cycle_data(user_id: int, priority: int) -> List[MovieCycle]:
     dao = MovieCycleDAO(constant_paths.CONFIG_FILE_PATH)
-    return dao.getRecordByUser(user_id)
+    return dao.getRecordByUserPriority(user_id, priority)
 
