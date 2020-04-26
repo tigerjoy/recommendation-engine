5b6211d20ffe652b8ec314db2a83d74dd3c2ef01
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-26 21:14:32 +0530

    Added priority in parameter

--- core/movieCycle.py
+++ core/movieCycle.py
@@ -1,9 +1,11 @@
 class MovieCycle:
 
-    def __init__(self, user_id: int = None, last_movie_id: int = None, second_last_movie_id: int = None):
+    def __init__(self, user_id: int = None, last_movie_id: int = None, second_last_movie_id: int = None,
+                 priority: int = None):
         self.user_id = user_id
         self.last_movie_id = last_movie_id
         self.second_last_movie_id = second_last_movie_id
+        self.priority = priority
 
     def getUserID(self):
         return self.user_id
@@ -14,6 +16,9 @@ class MovieCycle:
     def getSecondLastMovieID(self):
         return self.second_last_movie_id
 
+    def getPriority(self):
+        return self.priority
+
     def setUserID(self, user_id):
         self.user_id = user_id
 
@@ -23,8 +28,13 @@ class MovieCycle:
     def setSecondLastMovieID(self, second_last_movie_id):
         self.second_last_movie_id = second_last_movie_id
 
+    def setPriority(self, priority):
+        self.priority = priority
+
     def __str__(self):
-        return f"User ID: {self.user_id} Last Movie ID: {self.last_movie_id} Second Last Movie ID: {self.second_last_movie_id}"
+        return (f"User ID: {self.user_id} Last Movie ID: {self.last_movie_id}\n" +
+                f"Second Last Movie ID: {self.second_last_movie_id} Priority: {self.getPriority()}")
+
 
 if __name__ == "__main__":
-    print(MovieCycle(1, 9, 10))
+    print(MovieCycle(1, 9, 10, 3))
