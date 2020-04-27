0f204c1af73674c20bb220e9bb309d12317a0e30
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-27 10:23:24 +0530

    Added calculateAverageRatingByMovie() to calculate the average rating of a movie, addRatingByUser() to add rating entry to ratings_out table, and updateRatingByUser() to update the rating entry corresponding to user_id

--- dao/ratingDAO.py
+++ dao/ratingDAO.py
@@ -3,10 +3,11 @@ import sqlite3
 from typing import List
 from core.rating import Rating
 
+
 class RatingDAO():
 
     # Constructor
-    def __init__(self, prop_file:str) -> None:
+    def __init__(self, prop_file: str) -> None:
         self.prop_file = prop_file
         # Get DB properties
         with open(prop_file, "r") as prop:
@@ -34,7 +35,7 @@ class RatingDAO():
     # Return a list of Rating objects
     # containing rows which have the movieId
     # column same as the argument
-    def searchByMovieID(self, movieId:int) -> List[Rating]:
+    def searchByMovieID(self, movieId: int) -> List[Rating]:
         output = []
         cursor = self.myConn.execute(f"SELECT * FROM {self.tableName} WHERE movieId = {movieId}")
         for row in cursor:
@@ -44,12 +45,52 @@ class RatingDAO():
     # Return a list of Rating objects
     # containing rows which have the userId
     # column same as the argument
-    def searchByUserID(self, userId:int) -> List[Rating]:
+    def searchByUserID(self, userId: int) -> List[Rating]:
         output = []
         cursor = self.myConn.execute(f"SELECT * FROM {self.tableName} WHERE userId = {userId}")
         for row in cursor:
             output.append(self.convertRowToRating(row))
         return output
+
+    # Calculates and returns the average rating of a movie
+    def calculateAverageRatingByMovie(self, movieId: int) -> float:
+        query = f"""
+                    SELECT SUM(rating) / COUNT(rating)
+                    FROM {self.tableName}
+                    WHERE movieId = {movieId};
+                """
+        row = self.myConn.execute(query).fetchone()
+        avgRating = row[0]
+        return avgRating
+
+    # Add a rating entry in the ratings_out table
+    # Returns True if insert operation is successful, False otherwise
+    def addRatingByUser(self, the_rating: Rating) -> bool:
+        query = f"""
+                    INSERT INTO '{self.tableName}' ('userId', 'movieId', 'rating') 
+                    VALUES (?, ?, ?);
+                """
+
+        data_tuple = (the_rating.getUserID(), the_rating.getMovieID(), the_rating.getRating())
+
+        self.myConn.execute(query, data_tuple)
+
+        self.myConn.commit()
+
+        return self.myConn.total_changes > 0
+
+    # Update a rating entry in the ratings_out table
+    # Returns True if update operation is successful, False otherwise
+    def updateRatingByUser(self, the_rating: Rating) -> bool:
+        query = f'''
+                    UPDATE {self.tableName}
+                    SET rating = {the_rating.getRating()}
+                    WHERE userId = {the_rating.getUserID()}
+                    AND movieId = {the_rating.getMovieID()}
+                 '''
+        self.myConn.execute(query)
+        self.myConn.commit()
+        return self.myConn.total_changes == 1
 
     # Return a Rating object by converting
     # a row list of SQLite to Rating(userId, movieId, rating)
@@ -63,6 +104,8 @@ class RatingDAO():
 if __name__ == "__main__":
     dao = RatingDAO("../config/db_properties.json")
 
+    # print(dao.calculateAverageRatingByMovie(1))
+
     # table = dao.getAllRating()
 
     # count = 0
@@ -72,8 +115,8 @@ if __name__ == "__main__":
     #     if count == 20:
     #         break
 
-    for row in dao.searchByUserID(1):
-        print(row)
+    # for row in dao.searchByUserID(1):
+    #     print(row)
 
     # for movie in dao.searchByTitle("Toy"):
     #     print(movie)
