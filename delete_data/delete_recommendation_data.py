71e473ddf75bf9814f58a5f1d695e2e38731bab1
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-28 01:13:40 +0530

    Added delete_all_recommendations() which deletes all recommendations for a user and delete_recommendations() which deletes specific recommedations of a user whose priority is present in the priority list

--- delete_data/delete_recommendation_data.py
+++ delete_data/delete_recommendation_data.py
@@ -1,9 +1,28 @@
 from dao.recommendationDAO import RecommendationDAO
 from core.recommendation import Recommendation
+from get_data import get_recommendation_data as grd
+from typing import List
 import constant_paths
 
 
-def delete_recommendation(user_id: int, priority: int):
+def delete_recommendation(user_id: int, priority: int) -> bool:
     dao = RecommendationDAO(constant_paths.CONFIG_FILE_PATH)
     the_recommendation = Recommendation(user_id, priority)
     return dao.deleteRecommendation(the_recommendation)
+
+
+def delete_all_recommendations(user_id: int) -> bool:
+    recommendations = grd.get_recommendation_data(user_id)
+    deleted_recommendations = 0
+    for priority in range(1, 4):
+        if delete_recommendation(user_id, priority):
+            deleted_recommendations += 1
+
+    return len(recommendations) == deleted_recommendations
+
+
+def delete_recommendations(user_id: int, priority_list: List[int]) -> bool:
+    result = True
+    for priority in priority_list:
+        result = result and delete_recommendation(user_id, priority_list)
+    return result
