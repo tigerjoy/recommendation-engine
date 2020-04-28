3e3afeb388aed6ca8dd8e62d2f19800f530f8a16
tigerjoy <36330852+tigerjoy@users.noreply.github.com>
2020-04-28 11:30:31 +0530

    Added LOG_FILE_PATH

--- constant_paths.py
+++ constant_paths.py
@@ -1,1 +1,2 @@
 CONFIG_FILE_PATH = 'C:\\Users\\Ranajoy\\PycharmProjects\\recommendation-engine\\config\\db_properties.json'
+LOG_FILE_PATH = 'C:\\Users\\Ranajoy\\PycharmProjects\\recommendation-engine\\logs'
